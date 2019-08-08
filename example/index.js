96f71f018163e8d873f83a94f91a2570dc2d40dd
andydlindsay@gmail.com <andydlindsay@gmail.com>
2019-08-07 20:04:40 -0700

    update javascript file

--- example/index.js
+++ example/index.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
   
-  $('#paragraph').text('new text');
+  // adding children
+  $('#list').append('<li>I am a new list item!</li>');
+  $('#list').prepend('<li>I am the first list item!</li>');
 
-  var obj = {
-    horoscope: 'my horoscope',
-    "date": 'August 7, 2019'
-  }
-  
+  // adding siblings
+  $('#middle-child').before('<li>I come before my siblings</li>');
+  $('#middle-child').after('<li>I come after my siblings</li>');
 
+  // removing and replacing
+  $('#middle-child').remove();
+  $('#list').replaceWith('<p>I am not a list</p>');
 
+  // html and text
+  var titleText = $('#title').html();
+  console.log('titleText:', titleText);
+  $('#title').html('Shiny New Title');
+  $('#paragraph').text('All new text has been added');
 
 });
